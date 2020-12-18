3aaec570ded7f937d962c45bf0d2bfe8a2dd928e
lcw-2 <74635217+lcw-2@users.noreply.github.com>
2020-12-18 13:36:27 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1066,7 +1066,7 @@ class taskCog(commands.Cog):
 										tmp_bossDateString[i] = bossDateString[i] = nextTime.strftime('%Y-%m-%d')
 										await self.bot.get_channel(channel).send("```" + bossData[i][0] + ' 멍 입니다.```')
 										embed = discord.Embed(
-											description= '```다음  ' + bossData[i][0] + ' ' + bossTimeString[i] + '입니다.```',
+											description= '```다음시간은  ' + bossData[i][0] + ' ' + bossTimeString[i] + ' 입니다.```',
 											color=0xff0000
 											)
 										await self.bot.get_channel(channel).send(embed=embed, tts=False)
@@ -3549,7 +3549,7 @@ class IlsangDistributionBot(commands.AutoShardedBot):
 						tmp_bossTimeString[i] = bossTimeString[i] = nextTime.strftime('%H:%M:%S')
 						tmp_bossDateString[i] = bossDateString[i] = nextTime.strftime('%Y-%m-%d')
 						embed = discord.Embed(
-								description= '```다음  ' + bossData[i][0] + ' ' + bossTimeString[i] + '입니다.```',
+								description= '```다음시간은 ' + bossData[i][0] + ' ' + bossTimeString[i] + ' 입니다.```',
 								color=0xff0000
 								)
 						await self.get_channel(channel).send(embed=embed, tts=False)
@@ -3598,7 +3598,7 @@ class IlsangDistributionBot(commands.AutoShardedBot):
 							tmp_bossTimeString[i] = bossTimeString[i] = temptime.strftime('%H:%M:%S')
 							tmp_bossDateString[i] = bossDateString[i] = temptime.strftime('%Y-%m-%d')
 							embed = discord.Embed(
-									description= '```다음 시간은 ' + bossData[i][0] + ' ' + bossTimeString[i] + '입니다.```',
+									description= '```다음시간은 ' + bossData[i][0] + ' ' + bossTimeString[i] + ' 입니다.```',
 									color=0xff0000
 									)
 							await self.get_channel(channel).send(embed=embed, tts=False)
@@ -3617,7 +3617,7 @@ class IlsangDistributionBot(commands.AutoShardedBot):
 								tmp_bossTimeString[i] = bossTimeString[i] = nextTime.strftime('%H:%M:%S')
 								tmp_bossDateString[i] = bossDateString[i] = nextTime.strftime('%Y-%m-%d')
 								embed = discord.Embed(
-										description= '```다음 시간은 ' + bossData[i][0] + ' ' + bossTimeString[i] + '입니다.```',
+										description= '```다음시간은 ' + bossData[i][0] + ' ' + bossTimeString[i] + ' 입니다.```',
 										color=0xff0000
 										)
 								await self.get_channel(channel).send(embed=embed, tts=False)
