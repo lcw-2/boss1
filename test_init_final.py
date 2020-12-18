fb7cd85ec69597c970c5d4aae5d84c5bc96d486a
lcw-2 <74635217+lcw-2@users.noreply.github.com>
2020-12-18 14:32:49 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -3549,8 +3549,7 @@ class IlsangDistributionBot(commands.AutoShardedBot):
 						tmp_bossTimeString[i] = bossTimeString[i] = nextTime.strftime('%H:%M:%S')
 						tmp_bossDateString[i] = bossDateString[i] = nextTime.strftime('%Y-%m-%d')
 						embed = discord.Embed(
-								description= '```bossData[i][0][컷] ' + 
-							#다음시간은 + ' ' + bossTimeString[i] + ' 입니다.```',
+								description= '```bossData[i][0][컷] ' + #다음시간은 + ' ' + bossTimeString[i] + ' 입니다.```',
 								color=0xff0000
 								)
 						await self.get_channel(channel).send(embed=embed, tts=False)
